f49f93cb885473288c757e7c0666ddea70201701
WangRex <503431920@qq.com>
2016-10-07 09:40:51 +0800

    Add initial

---
+++ src/test/Test.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String str = String.valueOf(-1);
+		System.out.println(str);
+		System.out.println("-1".equals(str));
+	}
+}
